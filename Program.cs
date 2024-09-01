53b04371c57765bb23d1239f1e8f450efeb695f8
abdullah-cognite <100700755+abdullah-cognite@users.noreply.github.com>
2024-09-01 21:01:07 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace OTAUpdateHelper;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string upgradeFileName = "upgrade.bin";
+        string serviceNameInRegistry = "my-service";
+
+        string existingServiceFileNameWindows = "my-service.exe";
+        string existingServiceFileNameUnix = "my-service";
+        string existingServiceFileName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? existingServiceFileNameWindows : existingServiceFileNameUnix;
+        
+        OTAUpdateHandler.CreateUpgrader( upgradeFileName, existingServiceFileName, serviceNameInRegistry);
+
+        while (true)
+        {
+            Console.WriteLine("Checking for updates...");
+            
+            // Check if the upgrade file exists
+            if (File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, upgradeFileName)))
+            {
+                Console.WriteLine("Update found! Restarting the application...");
+                try{
+                    OTAUpdateHandler.PerformUpgrade();
+                    Environment.Exit(0);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Update failed: {ex.Message}");
+                }
+            }
+
+            // Delay before checking again
+            await Task.Delay(10000); // Wait for 10 seconds
+        }
+    }
+}
