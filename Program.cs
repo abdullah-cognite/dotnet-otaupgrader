b86f64b5df5f728da24ea17791d1e3fddf4c62f0
abdullah-cognite <100700755+abdullah-cognite@users.noreply.github.com>
2024-09-01 21:01:49 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
-namespace OTAUpdateHelper;
+namespace OTAUpdate;
 class Program
 {
     static async Task Main(string[] args)
